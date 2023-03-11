4ed8887fa072f3b530cd1315de87fc1b6900d8d1
users <kalmatovazarina1@gmail.com>
2023-03-11 15:34:00 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("erlan");
-        System.out.println("zaina");
+        System.out.println("Nurdolot");
     }
 }
