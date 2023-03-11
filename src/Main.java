d90d8b66e05549274251a029f46cc6c7f11f8a74
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-11 15:39:06 +0600

    Nurdolot commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nurdolot");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int suma = a*b;
+        System.out.println(suma);
     }
 }
