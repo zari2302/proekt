a8fe094a338252520e6e271d8371ace1a635c91f
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-14 14:55:33 +0600

    Nurdolot commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" бир задача жонотун ");
+
+
+
+
+
         System.out.println("Nurdolot");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int suma = a*b;
-        System.out.println(suma);
+        Nurdolot nurdolot = new Nurdolot();
+        nurdolot.name = "Nurdolot";
+        nurdolot.surmane = "Mamasabirov";
+        nurdolot.surnamee = "Jumabekovich";
+        nurdolot.age = 16;
+        nurdolot.ilivein = "Kalinin";
+        nurdolot.meshkoll = 28;
+        nurdolot.kurs = "it";
+        nurdolot.rodilsa = "kochkor-ata";
+        nurdolot.meheight = 182;
+        nurdolot.favoriteSport = "footbol";
+        nurdolot.favoriteSubject = "informatika";
+        nurdolot.FavoriteColor = "blue";
+        nurdolot.FavoriteFood = "plov";
+        nurdolot.meTelefon = "Poco x3 pro";
+        nurdolot.meNoutbook ="MacBook Air";
+        nurdolot.FavoriteMusic = "Healdlight";
+        nurdolot.FavoriteDidjei = "alok,alan wolker";
+        System.out.println("name: "+nurdolot.name+"\n"+"surmane: "+nurdolot.surmane+"\n " +"surnamee: "+ nurdolot.surnamee+"\n"+"age: "+nurdolot.age+"\n"+"ilivein: "+nurdolot.ilivein
+                +"\n"+"meshkoll: "+nurdolot.meshkoll+"\n"+"kurs: "+nurdolot.kurs+"\n"+"rodilsa: "+nurdolot.rodilsa+"\n"+"maheight: "+nurdolot.meheight+"\n"+"favoriteSport: "+nurdolot.favoriteSport
+                +"\n"+"favoriteSubject: "+nurdolot.favoriteSubject+"\n"+"FavoriteColor: "+nurdolot.FavoriteColor+"\n"+"FavoriteFood:" +nurdolot.FavoriteFood+"\n"+"meTelefon: "+nurdolot.meTelefon+"\n"
+                +"meNoutbook: "+nurdolot.meNoutbook+"\n"+"FavoriteMusic: "+nurdolot.FavoriteMusic+"\n"+"FavoriteDidjei: "+nurdolot.FavoriteDidjei);
+
+
 
     }
 }
---
+++ src/Nurdolot.java
@@ -0,0 +1,19 @@
+public class Nurdolot {
+    String  name;
+    String surnamee;
+    String surmane;
+    int age;
+    String ilivein;
+    int  meshkoll;
+    String kurs;
+    String rodilsa;
+    int  meheight;
+    String favoriteSport;
+    String favoriteSubject;
+    String FavoriteColor;
+    String FavoriteFood;
+    String meTelefon;
+    String meNoutbook;
+    String FavoriteMusic;
+    String FavoriteDidjei;
+}
